023a6c186b31aba7b068d19c0b2c13a742f2067a
Pramudi99 <sripramudi95@gmail.com>
2026-09-13 17:19:11 +0530

    fix: allow production frontend origin

--- EnhanzerAssignment.API/Program.cs
+++ EnhanzerAssignment.API/Program.cs
@@ -92,7 +92,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular", policy =>
     {
         policy
-            .WithOrigins("http://localhost:4200", "https://enhanzer-assignment-kfvqs80fl-pramudi.vercel.app" )
+            .WithOrigins("http://localhost:4200", "https://enhanzer-assignment-lilac.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
